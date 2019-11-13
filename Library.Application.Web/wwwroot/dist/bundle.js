c694c14ca9efc2a3fabb23ddd853e5e940a7c97c
E Versteeg <e.versteeg@solviteers.nl>
2019-11-13 15:10:45 +0100

    commit

--- Library.Application.Web/wwwroot/dist/bundle.js
+++ Library.Application.Web/wwwroot/dist/bundle.js
@@ -98,7 +98,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 //NPM packages
 __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
-__webpack_require__(/*! expose-loader?$!jquery */ "./node_modules/expose-loader/index.js?$!./node_modules/jquery/dist/jquery.js");
+__webpack_require__(/*! expose-loader?$!jquery */ "./node_modules/expose-loader/index.js?$!./node_modules/jquery/dist/jquery.js-exposed");
 __webpack_require__(/*! jquery-ajax-unobtrusive */ "./node_modules/jquery-ajax-unobtrusive/dist/jquery.unobtrusive-ajax.js");
 __webpack_require__(/*! jquery-validation */ "./node_modules/jquery-validation/dist/jquery.validate.js");
 __webpack_require__(/*! jquery-validation-unobtrusive */ "./node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js");
@@ -121,9 +121,9 @@ __webpack_require__(/*! datatables.net-buttons-bs4/css/buttons.bootstrap4.css */
 __webpack_require__(/*! datatables.net-select-bs4/css/select.bootstrap4.css */ "./node_modules/datatables.net-select-bs4/css/select.bootstrap4.css");
 __webpack_require__(/*! datatables.net-responsive-bs4/css/responsive.bootstrap4.css */ "./node_modules/datatables.net-responsive-bs4/css/responsive.bootstrap4.css");
 //Application scripts
-__webpack_require__(/*! expose-loader?Layout!./../Views/Shared/Layout/_Layout.ts */ "./node_modules/expose-loader/index.js?Layout!./Views/Shared/Layout/_Layout.ts");
-__webpack_require__(/*! expose-loader?Users!./../Views/Users/Users.ts */ "./node_modules/expose-loader/index.js?Users!./Views/Users/Users.ts");
-__webpack_require__(/*! expose-loader?Books!./../Views/Books/Books.ts */ "./node_modules/expose-loader/index.js?Books!./Views/Books/Books.ts");
+__webpack_require__(/*! expose-loader?Layout!./../Views/Shared/Layout/_Layout.ts */ "./node_modules/expose-loader/index.js?Layout!./Views/Shared/Layout/_Layout.ts-exposed");
+__webpack_require__(/*! expose-loader?Users!./../Views/Users/Users.ts */ "./node_modules/expose-loader/index.js?Users!./Views/Users/Users.ts-exposed");
+__webpack_require__(/*! expose-loader?Books!./../Views/Books/Books.ts */ "./node_modules/expose-loader/index.js?Books!./Views/Books/Books.ts-exposed");
 //Application style
 __webpack_require__(/*! ./Css/style.scss */ "./Resources/Css/style.scss");
 
@@ -30037,10 +30037,10 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*! DataTables 1
 
 /***/ }),
 
-/***/ "./node_modules/expose-loader/index.js?$!./node_modules/jquery/dist/jquery.js":
-/*!***************************************************************************!*\
-  !*** ./node_modules/expose-loader?$!./node_modules/jquery/dist/jquery.js ***!
-  \***************************************************************************/
+/***/ "./node_modules/expose-loader/index.js?$!./node_modules/jquery/dist/jquery.js-exposed":
+/*!***********************************************************************************!*\
+  !*** ./node_modules/expose-loader?$!./node_modules/jquery/dist/jquery.js-exposed ***!
+  \***********************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -30049,10 +30049,10 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*! DataTables 1
 
 /***/ }),
 
-/***/ "./node_modules/expose-loader/index.js?Books!./Views/Books/Books.ts":
-/*!*****************************************************************!*\
-  !*** ./node_modules/expose-loader?Books!./Views/Books/Books.ts ***!
-  \*****************************************************************/
+/***/ "./node_modules/expose-loader/index.js?Books!./Views/Books/Books.ts-exposed":
+/*!*************************************************************************!*\
+  !*** ./node_modules/expose-loader?Books!./Views/Books/Books.ts-exposed ***!
+  \*************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -30061,10 +30061,10 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*! DataTables 1
 
 /***/ }),
 
-/***/ "./node_modules/expose-loader/index.js?Layout!./Views/Shared/Layout/_Layout.ts":
-/*!****************************************************************************!*\
-  !*** ./node_modules/expose-loader?Layout!./Views/Shared/Layout/_Layout.ts ***!
-  \****************************************************************************/
+/***/ "./node_modules/expose-loader/index.js?Layout!./Views/Shared/Layout/_Layout.ts-exposed":
+/*!************************************************************************************!*\
+  !*** ./node_modules/expose-loader?Layout!./Views/Shared/Layout/_Layout.ts-exposed ***!
+  \************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -30073,10 +30073,10 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*! DataTables 1
 
 /***/ }),
 
-/***/ "./node_modules/expose-loader/index.js?Users!./Views/Users/Users.ts":
-/*!*****************************************************************!*\
-  !*** ./node_modules/expose-loader?Users!./Views/Users/Users.ts ***!
-  \*****************************************************************/
+/***/ "./node_modules/expose-loader/index.js?Users!./Views/Users/Users.ts-exposed":
+/*!*************************************************************************!*\
+  !*** ./node_modules/expose-loader?Users!./Views/Users/Users.ts-exposed ***!
+  \*************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
